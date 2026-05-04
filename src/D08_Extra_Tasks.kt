f48ba92c8695c40b5867a1fdb00eee19cad56e76
dipuhalder <dipuh2954@gmail.com>
2026-05-04 05:51:35 +0530

    practiced some tasks on Day 08 of mastering kotlin & Android App Development Journey

--- src/D08_Extra_Tasks.kt
+++ src/D08_Extra_Tasks.kt
@@ -4,23 +4,35 @@ fun main() {
 
 //    1. Declare a nullable String and print its length using safe call. 
     val name: String? = readlnOrNull()
-    var displayName: String? = name?.length?.toString()
-    println(displayName)
+    println(name?.length)
 //    2. Use Elvis operator to print "No Name" if name is null.
-    displayName = name ?: "No Name"
+    println(name ?: "No Name")
 //    3. Convert: username!!.length → safe call version.
-    val safename = name!!
-    
+    username?.length
 //    4. What is wrong with: val x = name.length if name is String??
+//    A String? can hold null , If you try to access using .length directly kotlin throws an error because it might throw an NullPointerException. name?.length must be used or a null check.
 //    5. Write an example where !! crashes your program.
+    val data: String? = null
+    println(data!!.length) // throws nullPointer exception
 //    6. Use let to print a value only if not null.
+    name?.let { println(it) }
 //    7. Use run to lowercase a nullable string.
+    name?.run { lowercase() }
 //    8. Combine ?. and ?: in one expression.
+    val length = name?.length ?: 0
 //    9. What does Int? mean? Write an example.
+    //It means either the result or Intput is either int or null
 //    10. Write a nullable Int and safely multiply it by 10.
+    val num: Int? = readlnOrNull()?.toInt()
+    val result = num?.times(10)
+    println("$length is $result")
 //    11. Write a chain like: email?.trim()?.lowercase()
+    val formatetedEmail = userEmail?.trim()?.lowercase()
 //    12. Convert a nullable list to size using safe calls.
+    val list: List<String>? = readlnOrNull()?.split(',')
+    val size = list?.size
 //    13. Use let to print the length of a nullable list.
+    
 //    14. Use run to operate on a nullable user object with multiple fields.
 //    15. Rewrite an unsafe expression to a fully null-safe one.
 //    16. Show a case where let is better than if (x != null).
