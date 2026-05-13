d05cb2ee326816ebe21db68e26e3a4b27175d6eb
dipuhalder <dipuh2954@gmail.com>
2026-05-13 07:41:17 +0530

    practiced some tasks on Day 09 of mastering kotlin & Android App Development Journey, But it's done along the way of learning new concepts so it named as Trial Programs.

--- src/W02D01_Tasks.kt
+++ src/W02D01_Tasks.kt
@@ -8,13 +8,29 @@ fun main() {
     shopping.add("Jeans")
     shopping.add("CellPhone")
 
-    shopping.removeAt(shopping.size / 2)
+    shopping.removeAt(2)
     shopping[0] = "Tablet"
 
     println(shopping)
+
     //Given a list of numbers [10, 3, 7, 1, 8, 5], sort it ascending using sort() and print. Then reverse and print again.
+    val numbers = mutableListOf(10, 3, 7, 1, 8, 5)
+    numbers.sort()
+    println(numbers)
+    numbers.reverse()
+    println(numbers)
 
     // Use withIndex() to print: "Position 0: Apple", "Position 1: Banana" etc. for a list of fruits.
+    shopping.withIndex().forEach { (index, fruit) ->
+        println("Position $index: $fruit")
+    }
     // Try accessing index 100 on a list of 3 items using both list[100] (catch the crash) and list.getOrNull(100) . Observe the difference.
+    try {
+        println(shopping[100])
+    } catch (e: IndexOutOfBoundsException) {
+        println("Caught an exception: ${e.message}")
+    }
+    println(shopping.getOrNull(100)) // This will print null instead of throwing an exception
+
 
 }
