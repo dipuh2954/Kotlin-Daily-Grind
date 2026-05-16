a5a17e07d1e3a40cdff115c00f46cc0100f2a97e
dipuhalder <dipuh2954@gmail.com>
2026-05-16 11:50:18 +0530

    practiced some tasks on Day 09 of mastering kotlin & Android App Development Journey, But it's done along the way of learning new concepts so it named as Trial Programs.

--- src/W02D01_Tasks.kt
+++ src/W02D01_Tasks.kt
@@ -1,36 +1,29 @@
 fun main() {
-    //Create an immutable list of 5 country names. Print the second and fourth country using index access.
-    val country = listOf("USA", "India", "France", "Germany", "Japan")
-    println(country[1])
-    //Create a mutable shopping list. Add 3 items, remove the middle one, then update the first item's name. Print the final list.
-    val shopping = mutableListOf("Bag", "Groceries", "Salad")
-    shopping.add("Shirt")
-    shopping.add("Jeans")
-    shopping.add("CellPhone")
-
-    shopping.removeAt(2)
-    shopping[0] = "Tablet"
-
-    println(shopping)
-
-    //Given a list of numbers [10, 3, 7, 1, 8, 5], sort it ascending using sort() and print. Then reverse and print again.
-    val numbers = mutableListOf(10, 3, 7, 1, 8, 5)
-    numbers.sort()
-    println(numbers)
-    numbers.reverse()
-    println(numbers)
-
-    // Use withIndex() to print: "Position 0: Apple", "Position 1: Banana" etc. for a list of fruits.
-    shopping.withIndex().forEach { (index, fruit) ->
-        println("Position $index: $fruit")
+// ================= 🟢List ===============
+//    Create an immutable list of 5 country names. Print the second and fourth country using index access.
+    val country = listOf("India", "Russia", "China", "japan", "Britain")
+    println(country[2])
+    println(country[4])
+//    Create a mutable shopping list. Add 3 items, remove the middle one, then update the first item's name. Print the final list.
+    val shopList = mutableListOf("Shirt", "Jeans", "Trousers", "T-shirt", "Hoodie")
+    println(shopList)
+    shopList.add("Chinos")
+    shopList.add("Shorts")
+    shopList.add("Cap")
+    println(shopList)
+    shopList.removeAt(3)
+    shopList[0] = "Socks"
+    println(shopList)
+//    Given a list of numbers [10, 3, 7, 1, 8, 5], sort it ascending using sort() and print. Then reverse and print again.
+    val num = listOf(10, 3, 7, 1, 8, 5)
+    println(num.sorted())
+//    Use withIndex() to print: "Position 0: Apple", "Position 1: Banana" etc. for a list of fruits.
+    val fruits = listOf("Apple", "Banana", "Guava", "Watermelon", "Orange")
+    for ((index, fruit) in fruits.withIndex()) {
+        println("Position: $index: $fruit")
     }
-    // Try accessing index 100 on a list of 3 items using both list[100] (catch the crash) and list.getOrNull(100) . Observe the difference.
-    try {
-        println(shopping[100])
-    } catch (e: IndexOutOfBoundsException) {
-        println("Caught an exception: ${e.message}")
-    }
-    println(shopping.getOrNull(100)) // This will print null instead of throwing an exception
-
+//    Try accessing index 100 on a list of 3 items using both list[100] (catch the crash) and list.getOrNull(100). Observe the difference.
+    println(fruits.getOrNull(100))
+    // In first case it gives compileTime error , and in second case it returns null.
 
 }
