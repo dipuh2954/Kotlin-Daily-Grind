504063a5c063de92814dd16d7b12c445e311d92d
dipuhalder <dipuh2954@gmail.com>
2026-06-02 06:13:38 +0530

    practiced some tasks on Day 09 of mastering kotlin & Android App Development Journey, But it's done along the way of learning new concepts so it named as Trial Programs.

--- src/kotlin_Basics.kt
+++ src/kotlin_Basics.kt
@@ -1,11 +1,10 @@
 fun main() {
     var age: UShort = 22u // Unsigned Integer : age cannot be negative no matter what
     println("My age is $age")
-    age = 23u // Unsigned means it can't be negative & there must be a u at the end of the value
+    age = 23u // Unsigned means it can't be negative & there must be an u at the end of the value
     println("My age now is $age")
-
-    //================ char,unicode and Backslash escape character =================
-    val myChar = 'a' // unicode characters it can be smilie too .. all kind of characters
+    //================ char,Unicode and Backslash escape character =================
+    val myChar = 'a' // Unicode characters it can be smilie too .... all kind of characters
     val new = '\u00AE'
     println("$myChar $new")
 }
